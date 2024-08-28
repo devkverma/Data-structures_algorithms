fda18b4f7a558f525fee7c4b52c89dfc0321991d
devkverma <devverma269@gmail.com>
2024-08-28 20:40:11 +0530

    Updated linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,5 +1,5 @@
 class ListNode:
-    def __init__(self , val):
+    def __init__(self , val = None):
         self.val = val
         self.next = None
 
@@ -13,10 +13,21 @@ class Solution:
         else:
             print(head.val,end=']')
 
-    def append(self,val):
-        pass
+    def append(self,linked_list,val):
+        head = linked_list
+        if head.val is None:
+            head.val = val
+            return
+        while head.next:
+            head = head.next
+        head.next = ListNode(val)
+        return
         
 
 sol = Solution()
+
+l1 = ListNode()
+sol.append(l1,5)
+sol.print(l1)
 
 
