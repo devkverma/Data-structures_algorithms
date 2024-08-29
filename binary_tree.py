341ff9ef5685ca60aa251883ce768b021f449300
devkverma <devverma269@gmail.com>
2024-08-29 18:12:29 +0530

    Updated binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -51,6 +51,53 @@ class Solution:
         self.postorder_r(root.rchild)
         print(root.val , end = ' ')
 
+    def preorder(self,tree):        # Preorder traversal using iteration
+        root = tree
+        if not root:
+            return
+        stack = []
+        result = []
+        stack.append(root)
+        while stack:
+            root = stack.pop()
+            result.append(root.val)
+            
+            if root.rchild: stack.append(root.rchild)
+            if root.lchild: stack.append(root.lchild)
+
+        return result
+    
+    def inorder(self,tree):         # Inorder traversal using iteration
+        root = tree
+        if not root:
+            return
+        stack = []
+        result = []
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.lchild
+            else:
+                root = stack.pop()
+                result.append(root.val)
+                root = root.rchild
+        return result
+    
+    def postorder(self,tree):
+        root = tree
+        if not root:
+            return
+        stack = [root]
+        result = []
+        while stack:
+            root = stack.pop()
+            if root:
+                result.append(root.val)
+                stack.append(root.lchild)
+                stack.append(root.rchild)
+        return result[::-1]
+            
+
 
 
 sol = Solution()
@@ -61,8 +108,7 @@ sol.addElement(t1,5)
 sol.addElement(t1,1)
 sol.addElement(t1,6)
 sol.addElement(t1,10)
-sol.inorder_r(t1)
-print()
-sol.preorder_r(t1)
-print()
+
 sol.postorder_r(t1)
+print()
+print(sol.postorder(t1))
